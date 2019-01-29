f91e8f99d3bacd2f9322223e728534bbde0f0625
Ralph Meier <ralphlmeier@gmail.com>
2019-01-29 17:06:11 +0100

    Update pizza

--- src/Pizza.java
+++ src/Pizza.java
@@ -2,20 +2,37 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Pizza {
-    private int rows, cols, minxNrOfIngredient, maxNrOfCells;
+    private int rows, cols, minNrOfIngredient, maxNrOfCells;
     private List<Slice> slices = new ArrayList<>();
 
     public List<Slice> getSlices() {
         return slices;
     }
 
-    public Pizza(int rows, int cols, int minxNrOfIngredient, int maxNrOfCells) {
+    public Pizza(int rows, int cols, int minNrOfIngredient, int maxNrOfCells) {
         this.rows = rows;
         this.cols = cols;
-        this.minxNrOfIngredient = minxNrOfIngredient;
+        this.minNrOfIngredient = minNrOfIngredient;
         this.maxNrOfCells = maxNrOfCells;
     }
+
+    public int getRows() {
+        return rows;
+    }
+
+    public int getCols() {
+        return cols;
+    }
+
+    public int getMinNrOfIngredient() {
+        return minNrOfIngredient;
+    }
+
+    public int getMaxNrOfCells() {
+        return maxNrOfCells;
+    }
 }
+
 
 
 
