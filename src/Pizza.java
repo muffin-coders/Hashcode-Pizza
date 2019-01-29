51b566059c9fa1ee96b8ed1e42ff5ea0004b9fbd
Ralph Meier <ralphlmeier@gmail.com>
2019-01-29 16:57:35 +0100

    Add constructor

--- src/Pizza.java
+++ src/Pizza.java
@@ -2,10 +2,20 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Pizza {
+    private int rows, cols, maxNrOfIngredient, maxNrOfCells;
     private List<Slice> slices = new ArrayList<>();
 
     public List<Slice> getSlices() {
         return slices;
     }
+
+    public Pizza(int rows, int cols, int maxNrOfIngredient, int maxNrOfCells) {
+        this.rows = rows;
+        this.cols = cols;
+        this.maxNrOfIngredient = maxNrOfIngredient;
+        this.maxNrOfCells = maxNrOfCells;
+    }
 }
-}
+
+
+
