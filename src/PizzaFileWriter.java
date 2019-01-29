3b839897c28e957ac8e2f5b87b4b7d8cebebc02e
Jennifer Schürch <jennifermschuerch@gmail.com>
2019-01-29 16:51:23 +0100

    Rename

---
+++ src/PizzaFileWriter.java
@@ -0,0 +1,20 @@
+import java.util.List;
+
+public class PizzaFileWriter {
+
+    private Pizza pizza;
+
+    public PizzaFileWriter(Pizza pizza) {
+        this.pizza = pizza;
+    }
+
+    public void generateOutput() {
+
+        List<Slice> slices = pizza.getSlices();
+
+        
+
+    }
+
+
+}
