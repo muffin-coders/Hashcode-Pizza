8226d2a46d0cfa3a4bcfbab9b6890f1974b7c10f
Jennifer Schürch <jennifermschuerch@gmail.com>
2019-01-29 17:29:52 +0100

    Add method to generate output file

--- src/PizzaFileWriter.java
+++ src/PizzaFileWriter.java
@@ -1,3 +1,9 @@
+import java.awt.*;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
 public class PizzaFileWriter {
@@ -8,13 +14,23 @@ public class PizzaFileWriter {
         this.pizza = pizza;
     }
 
-    public void generateOutput() {
+    public void generateOutput() throws IOException {
 
         List<Slice> slices = pizza.getSlices();
 
-        
+        String fileData = slices.size() + "\n";
+
+        for(Slice slice : slices) {
+            Point col = slice.getColumns();
+            Point row = slice.getRows();
+            fileData += row.x + " " + col.x + " " + row.y + " " + col.y + "\n";
+        }
+
+        FileOutputStream fos = new FileOutputStream("output.txt");
+        fos.write(fileData.getBytes());
+        fos.flush();
+        fos.close();
 
     }
-
 
 }
