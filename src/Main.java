1bfe87fe6a5d4a64af84ce58d3407c50c8be766e
Pascal Andermatt <p.andermatt@me.com>
2019-01-29 17:57:44 +0100

    Change main

--- src/Main.java
+++ src/Main.java
@@ -4,18 +4,18 @@ public class Main {
 
     public static void main(String[] args) throws IOException {
         String[] files = {"a_example.in", "b_small.in", "c_medium.in", "d_big.in"};
+        PizzaSlicer pizzaSlicerStrategie = new StupidPizzaSlicer();
 
         for (String file : files) {
             InputReader inputReader = new InputReader(file);
-            PizzaFileWriter pizzaFileWriter = new PizzaFileWriter(file);
+            PizzaFileWriter pizzaFileWriter =
+                    new PizzaFileWriter(pizzaSlicerStrategie.getClass().getName() + "_" + file);
 
             // Call Input Reader
             Pizza pizza = inputReader.read();
 
             // Pizza Slicing
-            PizzaSlicer pizzaSlicer = new StupidPizzaSlicer();
-
-            pizzaSlicer.cutPizza(pizza);
+            pizzaSlicerStrategie.cutPizza(pizza);
 
             // Call output writer
             pizzaFileWriter.generateOutput(pizza);
